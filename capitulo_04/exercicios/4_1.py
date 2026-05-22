1bcff9ee9fc973af6b6371c782c7f2373502cf98
Renato Novais <renatonovais.ps@gmail.com>
2026-05-22 01:03:47 -0300

    Estrutura inicial do curso de Python Perkovic

---
+++ capitulo_04/exercicios/4_1.py
@@ -0,0 +1,20 @@
+# Comece executando a atribuição:
+
+s = '0123456789'
+
+# Agora, escreva expressões (usando s e o operador de indexação) que sejam avaliadas como:
+
+print(s[2:-5])
+# (a)'234'
+
+print(s[7:-1])
+# (b)'78'
+
+print( s[0:-2] )
+# (c)'1234567'
+
+print(s[:-6])
+# (d)'0123'
+
+print(s[7:])
+# (e)'789'
