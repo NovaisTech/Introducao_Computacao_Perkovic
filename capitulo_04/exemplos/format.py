5e7aee92d9faa2236d2c37695dcb96ba0659ce2c
Renato Novais <renatonovais.ps@gmail.com>
2026-06-02 04:51:29 -0300

    Estrutura inicial do curso de Python Perkovic

---
+++ capitulo_04/exemplos/format.py
@@ -0,0 +1,13 @@
+diasemana = 'Quarta'
+mês = 'Outubro'
+dia = 9
+ano = 2013
+hora = 11
+minuto = 45
+segundo = 33
+
+
+print(diasemana+', '+str(dia)+' de ' +mês+ ',' +str(ano)
++' às '+str(hora)+ ' : '+str(minuto)+':'+str(segundo))
+
+print('{0}:{1}:{2}'.format(hora, minuto, segundo))
---
+++ capitulo_04/exercicios/4_5.py
@@ -0,0 +1,23 @@
+# Problema Prático 4.5
+
+# Suponha que as variáveis primeiro, último, rua, número, cidade, estado, codPostal
+# já tenham sido atribuídas. 
+# Escreva uma instrução print que crie uma etiqueta de correspondência:
+
+# John Doe
+
+# 123 Main Street
+
+# AnyCity, AS 09876
+
+# supondo que:
+
+primeiro = 'John' #{0}
+último = 'Doe' #{1}
+rua = 'Main Street' #{2}
+número = 123  #{3}
+cidade = 'AnyCity'  #{4}
+estado = 'AS'   #{5}
+codPostal = '09876'  #{6}
+
+print('{0} {1}\n{3} {2}\n{4}, {5} {6}'.format(primeiro, último,rua, número, cidade, estado, codPostal))
