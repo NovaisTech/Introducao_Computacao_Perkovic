f94a0742aa85396eae44f3702b7acfb91808e67b
Renato Novais <renatonovais.ps@gmail.com>
2026-06-03 03:24:57 -0300

    Estrutura inicial do curso de Python Perkovic

---
+++ capitulo_05/exercicios/5.1.py
@@ -0,0 +1,15 @@
+# Problema Prático 5.1
+
+# Implemente a função meuIMC(), que aceita como entrada a altura de uma pessoa (em metros) e o peso (em quilos) e calcula o Índice de Massa Corporal (IMC) dessa pessoa. A fórmula do IMC é:
+
+
+
+# Sua função deverá exibir a string 'Abaixo do peso' se o imc < 18.5, 'Normal' se 18,5 <= imc < 25, e 'Sobrepeso' se imc >= 25.
+
+# >>> meuIMC *86, 1.90)
+
+# Normal
+
+# >>> meuIMC (63, 1.90)
+
+# Abaixo do peso
