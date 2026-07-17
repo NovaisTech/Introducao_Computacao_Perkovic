200fb860c5f6d7e32185a8c2558c0b335fa7aa85
Renato Novais <renatonovais.ps@gmail.com>
2026-07-17 02:23:23 -0300

    Estrutura inicial do curso de Python Perkovic

--- capitulo_04/exemplos/FuncoesBasico.py
+++ capitulo_04/exemplos/FuncoesBasico.py
@@ -1,86 +1,89 @@
 
-# Exercícios de Funções em Python - Nível Iniciante
+# # Exercícios de Funções em Python - Nível Iniciante
 
-# Instruções:
-# Resolva cada exercício preenchendo o corpo da função.
-# Para testar, basta executar o arquivo no VS Code.
+# # Instruções:
+# # Resolva cada exercício preenchendo o corpo da função.
+# # Para testar, basta executar o arquivo no VS Code.
 
 
 
 # 1. Crie uma função chamada 'saudar' que retorne a frase "Olá, Mundo!"
 def saudar():
-    print("Olá, mundo!")
-saudar()   
+    return "Olá, mundo!"
+
+print(saudar()) 
 
 # 2. Crie uma função chamada 'dobro' que receba um número e retorne o dobro dele.
 def dobro(n):
-    dobrado = n + n
-    print(dobrado)
-
-dobro(50)
+    return n + n
+   
+print(dobro(50))
 
 # 3. Crie uma função 'soma' que receba dois números e retorne a soma deles.
-# def soma(n1, n2):
 def soma(n1, n2):
-    resultado = n1 + n1
-    print(resultado)
-soma(14 , 15)    
+    return n1 + n2
+   
+print(soma(14 , 15))    
 
 
 
 # 4. Crie uma função 'area_retangulo' que receba base e altura e retorne a área.
 # Seudef area_retangulo(base, altura):
 def area_retangulo(base, altura):
-    area = base * altura /2
-    print(area)
-area_retangulo(3 , 5)    
+    return base * altura /2
+
+print(area_retangulo(3 , 5))
 
 
-# 5. Crie uma função 'e_par' que receba um número e retorne True se for par e False se for ímpar.
+
+# # 5. Crie uma função 'e_par' que receba um número e retorne True se for par e False se for ímpar.
 def even(n):
     if n % 2 == 0:
-        print("É par")
-    else:
-        print("não é par!") 
-even(10)           
+        return "É par!"
+    
+    return "não é par!" # dispensa uso do Else, mas precisa manter a indentação 
+print(even(101))         
 
 
 
-# 6. Crie uma função 'maior_numero' que receba dois números e retorne o maior deles.
+# # 6. Crie uma função 'maior_numero' que receba dois números e retorne o maior deles.
 def maiorNumero(a, b):
     if a > b:
-        print(f" o número {a} é maior que {b}")
-    else:
-        print(f"O núemro {b} é maior que {a}")
-maiorNumero(23, 5)     
+        return f" o número {a} é maior que {b}"
+    
+    return    f"O núemro {b} é maior que {a}"
+print(maiorNumero(23, 5))     
 
 
        
-# 7. Crie uma função 'contar_letras' que receba uma palavra e retorne a quantidade de caracteres dela.
+# # 7. Crie uma função 'contar_letras' que receba uma palavra e retorne a quantidade de caracteres dela.
 def contar_letras(palavra):
-    print(len(palavra))
+    return len(palavra)
     
-contar_letras("Paralelepipedo")        
+print(contar_letras("Paralelepipedo"))        
 
 
-# 8. Crie uma função 'converter_celsius' que receba uma temperatura em Celsius e retorne em Fahrenheit.
-# Dica: (C * 9/5) + 32
+# # 8. Crie uma função 'converter_celsius' que receba uma temperatura em Celsius e retorne em Fahrenheit.
+# # Dica: (C * 9/5) + 32
 def conversor_temperatura(tempC):
     fahrenheit = ((tempC * 9 ) / 5 ) + 32
-    print(f"A temperatura em {tempC} convertida para é igual: ", fahrenheit)
-conversor_temperatura(18)    
+    
+    return f"A temperatura em {tempC} convertida para fahrenheit é igual: {fahrenheit} "
+    # return f"A temperatura em {tempC} convertida para fahrenheit é {((tempC * 9 ) / 5 ) + 32}"
+print(conversor_temperatura(18))    
 
 
-# 9. Crie uma função 'saudacao_personalizada' que receba um nome e retorne "Olá, [nome]! Seja bem-vindo."
+# # 9. Crie uma função 'saudacao_personalizada' que receba um nome e retorne "Olá, [nome]! Seja bem-vindo."
 def saudacao_personalizada(nome):
-    print(f"Olá, {nome} seja bem-vindo")
-saudacao_personalizada("Fábio")    
+    return f"Olá, {nome} seja bem-vindo"
+
+print(saudacao_personalizada("Fábio"))    
   
   
     
-# 10. Crie uma função 'calcular_desconto' que receba o valor de um produto e a porcentagem de desconto.
-# A função deve retornar o valor final com o desconto aplicado.  
+# # 10. Crie uma função 'calcular_desconto' que receba o valor de um produto e a porcentagem de desconto.
+# # A função deve retornar o valor final com o desconto aplicado.  
 def calcular_desconto(valor_produto):
        desconto = valor_produto * 0.9
-       print(f"O valor do produto com desconto de 10 % é igual {desconto}")
-calcular_desconto(59)       
+       return f"O valor do produto com desconto de 10 % é igual {desconto}"
+print(calcular_desconto(59))       
